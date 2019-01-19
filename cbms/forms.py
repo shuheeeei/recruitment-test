f3a52c5662fc8bdedf1ec6bb7442248c863c17ed
shuheeeei <shuheeeei.kawashima@gmail.com>
2019-01-19 20:05:11 +0900

    受講履歴登録機能の実装
    view関数、フォームの生成

--- cbms/forms.py
+++ cbms/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from .models import Costomer
+from .models import Costomer, History, Genre
 
 
 class CostomerForm(forms.ModelForm):
     class Meta:
         model = Costomer
         fields = ['name', 'gender', 'age']
+
+class HistoryForm(forms.ModelForm):
+    class Meta:
+        model = History
+        fields = ['costomer', 'genre', 'date', 'time',]
+    costomer = forms.ModelChoiceField(
+        queryset=Costomer.objects.all(),
+        label='顧客名'
+    )
+    genre = forms.ModelChoiceField(
+        queryset=Genre.objects.all(),
+        label='ジャンル'
+    )
