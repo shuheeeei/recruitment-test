8596042295ecdbaa1ed10935ad7c14fdb2a9e41f
shuheeeei <shuheeeei.kawashima@gmail.com>
2019-01-19 11:35:18 +0900

    顧客一覧の画面の作成

--- cbms/views.py
+++ cbms/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Costomer
+from django.shortcuts import redirect
+from .forms import CostomerForm
+
 
 def index(request):
     params = {
             'title': 'CostomerBillingManagementSystem'
     }
     return render(request, 'cbms/index.html', params)
+
+def costomers(request):
+    data = Costomer.objects.all()
+    params = {
+            'title': 'Costomer List',
+            'data': data,
+    }
+    return render(request, 'cbms/costomer_list.html', params)
+
+def costomer_create(request):
+    if request.method == 'POST':
+        obj = Costomer()
+        costomer = CostomerForm(request.POST, instance=obj)
+        costomer.save()
+        return redirect(to='/cbms/costomers')
+    params = {
+            'title': 'Costomer Create',
+            'form': CostomerForm(),
+    }
+    return render(request, 'cbms/costomer_create.html', params)
